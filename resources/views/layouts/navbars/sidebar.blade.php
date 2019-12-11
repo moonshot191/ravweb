89cfca7407324a9a6d82a064b095c3ea0fbccea0
iameasyguy <thebotsguy@gmail.com>
2019-12-11 17:24:04 +0300

    updated permissions view

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -11,13 +11,13 @@
   </div>
   <div class="sidebar-wrapper">
     <ul class="nav">
-      <li class="nav-item{{ $activePage ?? '' ?? '' == 'dashboard' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('home') }}">
-          <i class="material-icons">dashboard</i>
-            <p>{{ __('Dashboard') }}</p>
-        </a>
-      </li>
-      <li class="nav-item {{ ($activePage ?? '' ?? '' == 'profile' || $activePage ?? '' ?? '' == 'user-management') ? ' active' : '' }}">
+        <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
+            <a class="nav-link" href="{{ route('home') }}">
+                <i class="material-icons">dashboard</i>
+                <p>{{ __('Dashboard') }}</p>
+            </a>
+        </li>
+        <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
         <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
             <i class="material-icons">account_box</i>
           <p>{{ __('Users & Permissions') }}
@@ -26,20 +26,20 @@
         </a>
         <div class="collapse" id="laravelExample">
           <ul class="nav">
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'profile' ? ' active' : '' }}">
+            <li class="nav-item{{ $activePage  == 'profile' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('profile.edit') }}">
                 <span class="sidebar-mini"> UP </span>
                 <span class="sidebar-normal">{{ __('User profile') }} </span>
               </a>
             </li>
               @can('add_users','edit_users', 'delete_users')
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'user-management' ? ' active' : '' }}">
+                  <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('users.index') }}">
                 <span class="sidebar-mini"> UM </span>
                 <span class="sidebar-normal"> {{ __('User Management') }} </span>
               </a>
             </li>
-                  <li class="nav-item{{ $activePage ?? '' ?? '' == 'user-management' ? ' active' : '' }}">
+                  <li class="nav-item{{ $activePage == 'roles-management' ? ' active' : '' }}">
                       <a class="nav-link" href="{{ route('roles.index') }}">
                           <span class="sidebar-mini"> RM </span>
                           <span class="sidebar-normal"> {{ __('Roles & Permissions') }} </span>
@@ -52,7 +52,7 @@
       </li>
 
         @can('add_apollo','edit_apollo', 'delete_apollo')
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'apollo-management' ? ' active' : '' }}">
+            <li class="nav-item{{ $activePage == 'apollo-management' ? ' active' : '' }}">
                 <a class="nav-link" href="{{ route('apollo.index') }}">
                     <i class="material-icons">help</i>
                     <p>{{ __('Apollo Management') }}</p>
@@ -61,7 +61,7 @@
         @endcan
 
         @can('add_seshats','edit_seshats', 'delete_seshats')
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'seshat-management' ? ' active' : '' }}">
+            <li class="nav-item{{ $activePage  == 'seshat-management' ? ' active' : '' }}">
                 <a class="nav-link" href="{{ route('seshats.index') }}">
                     <i class="material-icons">add_a_photo</i>
                     <p>{{ __('Seshat Management') }}</p>
@@ -71,7 +71,7 @@
 
 
         @can('add_zalmos','edit_zalmos', 'delete_zalmos')
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'seshat-management' ? ' active' : '' }}">
+            <li class="nav-item{{ $activePage  == 'seshat-management' ? ' active' : '' }}">
                 <a class="nav-link" href="{{ route('zalmos.index') }}">
                     <i class="material-icons">mic</i>
                     <p>{{ __('Zalmoxis Management') }}</p>
@@ -79,7 +79,7 @@
             </li>
         @endcan
         @can('add_gaias','edit_gaias', 'delete_gaias')
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'gaia-management' ? ' active' : '' }}">
+            <li class="nav-item{{ $activePage  == 'gaia-management' ? ' active' : '' }}">
                 <a class="nav-link" href="{{ route('gaias.index') }}">
                     <i class="material-icons">record_voice_over</i>
                     <p>{{ __('Gaia Management') }}</p>
@@ -87,7 +87,7 @@
             </li>
         @endcan
         @can('add_africas','edit_africas', 'delete_africas')
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'africa-management' ? ' active' : '' }}">
+            <li class="nav-item{{ $activePage  == 'africa-management' ? ' active' : '' }}">
                 <a class="nav-link" href="{{ route('africas.index') }}">
                     <i class="material-icons">swap_horizontal_circle</i>
                     <p>{{ __('Africa Management') }}</p>
@@ -95,7 +95,7 @@
             </li>
         @endcan
         @can('add_leizis','edit_leizis', 'delete_leizis')
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'leizi-management' ? ' active' : '' }}">
+            <li class="nav-item{{ $activePage  == 'leizi-management' ? ' active' : '' }}">
                 <a class="nav-link" href="{{ route('leizis.index') }}">
                     <i class="material-icons">compare_arrows</i>
                     <p>{{ __('Leizi Management') }}</p>
@@ -103,7 +103,7 @@
             </li>
         @endcan
         @can('add_groups','edit_groups', 'delete_groups')
-            <li class="nav-item{{ $activePage ?? '' ?? '' == 'groups-management' ? ' active' : '' }}">
+            <li class="nav-item{{ $activePage  == 'groups-management' ? ' active' : '' }}">
                 <a class="nav-link" href="{{ route('groups.index') }}">
                     <i class="material-icons">groups-management</i>
                     <p>{{ __('Groups Management') }}</p>
